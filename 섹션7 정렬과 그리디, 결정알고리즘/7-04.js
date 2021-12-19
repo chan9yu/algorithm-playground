ada47b6af16e05ac397aadf92547aecf32558594
Chan Gyu <wry5533@gmail.com>
2021-12-19 19:40:28 +0900

    섹션 7 완료!

---
+++ 섹션7 정렬과 그리디, 결정알고리즘/7-04.js
@@ -0,0 +1,20 @@
+// 4. 삽입 정렬
+// N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+// 정렬하는 방법은 삽입정렬입니다.
+
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
+  }
+  return answer;
+}
+
+let arr = [11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
