a61b2c75ab692ae52224b1711a5c1762019c1cd3
changyuyeo <wry5533@gmail.com>
2022-01-24 20:19:19 +0900

    색션2 복습

--- 섹션2 2차원 배열 탐색/2-06.js
+++ 섹션2 2차원 배열 탐색/2-06.js
@@ -1,10 +1,44 @@
-// 6. 격자판 최대합
-// N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력하세요.
+//? 6. 격자판 최대합
+//* 5*5 격자판에 아래롸 같이 숫자가 적혀있습니다.
+//* 10 13 10 12 15
+//* 12 39 30 23 11
+//* 11 25 50 53 15
+//* 19 27 29 37 27
+//* 19 13 30 13 19
+//* N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가장 큰 합을 출력하세요.
 
 function solution(arr) {
-	let answer
+	let answer = Number.MIN_SAFE_INTEGER
+	let n = arr.length
+	let sum1 = 0 //memo 행의 합계를 담는 변수
+	let sum2 = 0 //memo 열의 합계를 담는 변수
+
+	//memo 1. 행과 열 합계중 제일 큰 값 찾기
+	for (let i = 0; i < n; i++) {
+		sum1 = sum2 = 0
+		for (let j = 0; j < n; j++) {
+			sum1 += arr[i][j] //memo 행 번호를 고정 후 값 누적
+			sum2 += arr[j][i] //memo 열 번호를 고정 후 값 누적
+		}
+		answer = Math.max(answer, sum1, sum2)
+	}
+
+	sum1 = sum2 = 0 //memo 두 대각선의 합계를 담는 변수
+	//memo 2. 두 대각선의 합계중 제일 큰 값 찾기
+	for (let i = 0; i < n; i++) {
+		sum1 += arr[i][i]
+		sum2 += arr[i][n - i - 1]
+	}
+	answer = Math.max(answer, sum1, sum2)
 
 	return answer
 }
 
+let arr = [
+	[10, 13, 10, 12, 15],
+	[12, 39, 30, 23, 11],
+	[11, 25, 50, 53, 15],
+	[19, 27, 29, 37, 27],
+	[19, 13, 30, 13, 19]
+]
 console.log(solution(arr))
