217e85364b85c8e4dc3dfe61d32591ae98d217c0
changyuyeo <wry5533@gmail.com>
2022-01-31 20:19:19 +0900

    섹션5 복습

--- 섹션5 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5-01.js
+++ 섹션5 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5-01.js
@@ -1,21 +1,25 @@
-// 1. 두 배열 합치기
-// 오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+//? 1. 두 배열 합치기
+//* 오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
 
 function solution(arr1, arr2) {
-  let answer = [];
-  let n = arr1.length;
-  let m = arr2.length;
-  let p1 = 0;
-  let p2 = 0;
-  while (p1 < n && p2 < m) {
-    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
-    else answer.push(arr2[p2++]);
-  }
-  while (p1 < n) answer.push(arr1[p1++]);
-  while (p2 < m) answer.push(arr2[p2++]);
-  return answer;
+	let answer = []
+	let n = arr1.length
+	let m = arr2.length
+	let p1 = 0
+	let p2 = 0
+
+	//memo 두 배열중 하나라도 탐색이 완료될 때 까지 반복
+	while (p1 < n && p2 < m) {
+		arr1[p1] <= arr2[p2] ? answer.push(arr1[p1++]) : answer.push(arr2[p2++])
+	}
+
+	//memo 탐색 종료 후 남은 배열에 값들 정리
+	while (p1 < n) answer.push(arr1[p1++])
+	while (p2 < m) answer.push(arr2[p2++])
+
+	return answer
 }
 
-let a = [1, 3, 5];
-let b = [2, 3, 6, 7, 9];
-console.log(solution(a, b));
+let a = [1, 3, 5]
+let b = [2, 3, 6, 7, 9]
+console.log(solution(a, b))
--- 섹션5 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5-08.js
+++ 섹션5 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5-08.js
@@ -1,41 +1,46 @@
-// 8. 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
-// S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하세요.
-// 아나그램 판별시 대소문자가 구분됩니다. 부분문자열은 연속된 문자열이어야 합니다.
+//? 8. 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
+//* S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하세요.
+//* 아나그램 판별시 대소문자가 구분됩니다.
+//* 부분문자열은 연속된 문자열이어야 합니다.
 
 function compareMaps(map1, map2) {
-  if (map1.size !== map2.size) return false;
-  for (let [key, val] of map1) {
-    if (!map2.has(key) || map2.get(key) !== val) return false;
-  }
-  return true;
+	//memo 키 종류가 다른지 검사
+	if (map1.size !== map2.size) return false
+	for (let [key, val] of map1) {
+		//memo map1에 있는 key가 map2에 있는지 또는 값이 서로 다른지 검사
+		if (!map2.has(key) || map2.get(key) !== val) return false
+	}
+	return true
 }
 
 function solution(s, t) {
-  let answer = 0;
-  let tH = new Map();
-  let sH = new Map();
-  for (let v of t) {
-    if (tH.has(v)) tH.set(v, tH.get(v) + 1);
-    else tH.set(v, 1);
-  }
-  let len = t.length - 1;
-  for (let i = 0; i < len; i++) {
-    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
-    else sH.set(s[i], 1);
-  }
-  let lt = 0;
-  for (let rt = len; rt < s.length; rt++) {
-    if (sH.has(s[rt])) sH.set(s[rt], sH.get(s[rt]) + 1);
-    else sH.set(s[rt], 1);
-    if (compareMaps(sH, tH)) answer += 1;
-    sH.set(s[lt], sH.get(s[lt]) - 1);
-    if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
-    lt += 1;
-  }
+	let answer = 0
+	let tH = new Map()
+	let sH = new Map()
 
-  return answer;
+	for (let v of t) tH.has(v) ? tH.set(v, tH.get(v) + 1) : tH.set(v, 1)
+
+	let len = t.length - 1
+	for (let i = 0; i < len; i++) {
+		sH.has(s[i]) ? sH.set(s[i], sH.get(s[i]) + 1) : sH.set(s[i], 1)
+	}
+
+	//memo 투포인터 & 슬라이딩 윈도우 (1. 추가 2. 비교 3. lt가 참조 중인 값 제거 후 1증가)
+	let lt = 0
+	for (let rt = len; rt < s.length; rt++) {
+		//memo 1. 추가
+		sH.has(s[rt]) ? sH.set(s[rt], sH.get(s[rt]) + 1) : sH.set(s[rt], 1)
+		//memo 2. 비교
+		if (compareMaps(sH, tH)) answer += 1
+		//memo 3. lt가 참조 중인 값 제거 후 1증가
+		sH.set(s[lt], sH.get(s[lt]) - 1) //* 값 1 감소 후,
+		sH.get(s[lt]) === 0 && sH.delete(s[lt]) //* 해당 키의 값이 0이면 delete
+		lt += 1 //* 제거 후 lt 1증가
+	}
+
+	return answer
 }
 
-let a = "bacaAacba";
-let b = "abc";
-console.log(solution(a, b));
+let a = 'bacaAacba'
+let b = 'abc'
+console.log(solution(a, b))
