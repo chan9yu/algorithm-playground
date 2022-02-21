2a99eeb5ef07bc4c60b6509f950543bff9fe08e7
changyuyeo <wry5533@gmail.com>
2022-02-21 20:19:19 +0900

    프로그래머스 1단계 문제 추가

--- programmers/1단계/이상한 문자 만들기.js
+++ programmers/1단계/이상한 문자 만들기.js
@@ -1,19 +1,22 @@
-// 문자열 s는 한 개 이상의 단어로 구성되어 있습니다.
-// 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
-// 각 단어의 짝수번째 알파벳은 대문자로,
-// 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+//? 이상한 문자 만들기
+//* 문자열 s는 한 개 이상의 단어로 구성되어 있습니다.
+//* 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+//* 각 단어의 짝수번째 알파벳은 대문자로,
+//* 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
 
 function solution(s) {
-  let answer = "";
-  let a = s.split(" ");
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a[i].length; j++) {
-      if (j % 2 !== 0) answer = answer + a[i][j].toLowerCase();
-      else answer = answer + a[i][j].toUpperCase();
-    }
-    if (i < a.length - 1) answer = answer + " ";
-  }
-  return answer;
+	let answer = ''
+	let a = s.split(' ')
+
+	for (let i = 0; i < a.length; i++) {
+		for (let j = 0; j < a[i].length; j++)
+			j % 2 !== 0
+				? (answer += a[i][j].toLowerCase())
+				: (answer += a[i][j].toUpperCase())
+		i < a.length - 1 && (answer += ' ')
+	}
+
+	return answer
 }
 
-console.log(solution("try hello world")); // "TrY HeLlO WoRlD"
+console.log(solution('try hello world')) //* return "TrY HeLlO WoRlD"
--- programmers/1단계/자릿수 더하기.js
+++ programmers/1단계/자릿수 더하기.js
@@ -1,13 +1,17 @@
-// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
-// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+//? 자릿수 더하기
+//* 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서
+//* return 하는 solution 함수를 만들어 주세요.
+//* 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
 
 function solution(n) {
-  let answer = 0;
-  for (let v of n.toString()) {
-    answer += Number(v);
-  }
-  return answer;
+	let answer = 0
+
+	n.toString()
+		.split('')
+		.forEach(e => (answer += parseInt(e, 10)))
+
+	return answer
 }
 
-console.log(solution(123)); // 1 + 2 + 3 = 6이므로 6를 return 하면 됩니다.
-console.log(solution(987)); // 9 + 8 + 7 = 24이므로 24를 return 하면 됩니다.
+console.log(solution(123)) //* return  6
+console.log(solution(987)) //* return  24
