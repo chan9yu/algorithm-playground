1359005704ca7ebde086ca0565425fb2073faefe
chan9yu <dev.cgyeo@gmail.com>
2025-03-03 20:30:24 +0900

    예전 파일 정리

--- baekjoon/step_1/1001.js
+++
@@ -1,8 +0,0 @@
-// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
-let fs = require("fs");
-// let input = fs.readFileSync("/dev/stdin").toString().split(" ");
-let input = fs.readFileSync("text.txt").toString().split(" ");
-let num1 = Number(input[0]);
-let num2 = Number(input[1]);
-
-console.log(num1 - num2);
