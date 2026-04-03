7dcdc9e13975921c514c00f86fcdb04c8181f4b6
여찬규 <dev.cgyeo@gmail.com>
2026-04-03 16:33:17 +0900

    💡 [LTC] Hash Table: 0387-first-unique-character-in-a-string
    - Time: 35 ms (44.03%)
    - Space: 60.5 MB (53.14%)
    - Difficulty: Easy
    - Date: 04-03-2026

---
+++ leetcode/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.ts
@@ -0,0 +1,15 @@
+function firstUniqChar(s: string): number {
+    const map = new Map();
+
+    for (let v of s) {
+        map.set(v, (map.get(v) ?? 0) + 1);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.get(s[i]) === 1) {
+            return i;
+        }
+    }
+
+    return -1;
+};
