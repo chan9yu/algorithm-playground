47001073ecd9851427eeb800ffaec33d24aa57cb
여찬규 <dev.cgyeo@gmail.com>
2026-04-05 15:33:27 +0900

    💡 [LTC] Array: 0136-single-number
    - Time: 12 ms (23.29%)
    - Space: 58.8 MB (46.68%)
    - Difficulty: Easy
    - Date: 04-05-2026

--- leetcode/0136-single-number/0136-single-number.ts
+++ leetcode/0136-single-number/0136-single-number.ts
@@ -1,16 +1,9 @@
 function singleNumber(nums: number[]): number {
-    const map = new Map();
-    let answer = 0;
+	nums.sort((a, b) => a - b);
 
-    for (const v of nums) {
-        map.set(v, (map.get(v) ?? 0) + 1);
-    }
-
-    map.forEach((v, k) => {
-        if (v === 1) {
-            answer = k;
-        }
-    })
-
-    return answer;
-};
+	for (let i = 0; i < nums.length; i += 2) {
+		if (nums[i] !== nums[i + 1]) {
+			return nums[i];
+		}
+	}
+}
