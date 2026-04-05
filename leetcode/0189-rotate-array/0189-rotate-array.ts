4f1b6e6cb2c2af23fa2e503966b747aadf741bac
여찬규 <dev.cgyeo@gmail.com>
2026-04-05 16:22:40 +0900

    💡 [LTC] Array: 0189-rotate-array
    - Time: 0 ms (100%)
    - Space: 65.3 MB (83.87%)
    - Difficulty: Medium
    - Date: 04-05-2026

--- leetcode/0189-rotate-array/0189-rotate-array.ts
+++ leetcode/0189-rotate-array/0189-rotate-array.ts
@@ -1,22 +1,24 @@
+function reverse(arr, left, right) {
+    while (left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]]
+        left += 1;
+        right -= 1;
+    }
+}
+
 /**
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-	const n = nums.length;
-	k = k % n;
-
-	function reverse(start: number, end: number): void {
-		while (start < end) {
-			[nums[start], nums[end]] = [nums[end], nums[start]];
-			start++;
-			end--;
-		}
-	}
+    let n = nums.length;
+    k = k % n;
 
 	// 1. 전체 배열 뒤집기
-	reverse(0, n - 1);
+    reverse(nums, 0, n - 1);
+
 	// 2. 앞쪽 k개 뒤집기
-	reverse(0, k - 1);
+    reverse(nums, 0, k - 1);
+
 	// 3. 나머지 뒤집기
-	reverse(k, n - 1);
-}
+    reverse(nums, k, n - 1);
+};
