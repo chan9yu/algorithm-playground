04c3a777118d23dab6589f21ce82c438588fb026
여찬규 <dev.cgyeo@gmail.com>
2026-04-05 15:52:33 +0900

    💡 [LTC] Array: 0026-remove-duplicates-from-sorted-array
    - Time: 0 ms (100%)
    - Space: 58.5 MB (78.27%)
    - Difficulty: Easy
    - Date: 04-05-2026

---
+++ leetcode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,14 @@
+function removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+
+    let write = 1;
+
+    for (let read = 1; read < nums.length; read++) {
+        if (nums[read] !== nums[write - 1]) {
+            nums[write] = nums[read];
+            write += 1;
+        }
+    }
+
+    return write;
+}
