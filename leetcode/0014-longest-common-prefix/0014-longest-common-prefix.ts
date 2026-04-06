79288c10a23241946e27b3ebc1f030106eb82921
여찬규 <dev.cgyeo@gmail.com>
2026-04-06 12:48:13 +0900

    💡 [LTC] Array: 0014-longest-common-prefix
    - Time: 1 ms (65.87%)
    - Space: 56.8 MB (37.62%)
    - Difficulty: Easy
    - Date: 04-06-2026

---
+++ leetcode/0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -0,0 +1,12 @@
+function longestCommonPrefix(strs: string[]): string {
+    const answer = strs[0];
+
+    for (let i = 0; i < answer.length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] === answer[i]) continue;
+            return answer.slice(0, i);
+        }
+    }
+
+    return answer;
+};
